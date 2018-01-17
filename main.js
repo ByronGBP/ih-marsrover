bf9b9fe75ae46cebda6073a41f7f3383ef15186c
ByronGBP <byrongbp@gmail.com>
2018-01-17 20:14:37 +0100

    setup

---
+++ main.js
@@ -0,0 +1,132 @@
+//TODO: Debugar: Rover 2 [8,0] Direction: E, con los movimientos actuales.
+//TODO: Automatizar el setup con valores randoms tanto para el grid, y los valores de los Rovers.
+//TODO: Crear un boton para poder realizar los setups.
+//TODO: CleanCode de los objetos Rover.
+
+var grid = [
+  [".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+  [".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+  [".", ".", ".", ".", ".", ".", ".", "X", ".", "."],
+  [".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+  [".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+  [".", ".", ".", ".", ".", "X", ".", ".", ".", "."],
+  [".", ".", ".", ".", ".", ".", ".", ".", "X", "."],
+  [".", "X", ".", ".", ".", ".", ".", ".", ".", "."],
+  [".", ".", ".", ".", ".", ".", "X", ".", ".", "."],
+  ["X", ".", ".", ".", ".", ".", ".", ".", ".", "."]
+];
+
+var gridDisplay = "";
+var movementsRover1 = "";
+var movementsRover2 = "";
+
+function doMovement(movement, rover) {
+
+  switch (movement) {
+    case 'f':
+      rover.goForward();
+      break;
+    case 'b':
+      rover.goBackward();
+      break;
+    case 'r':
+      rover.turnRigth();
+      break;
+    case 'l':
+      rover.turnLeft();
+      break;
+  }
+
+  rover.showReport();
+
+}
+
+function displayGrid() {
+
+  gridDisplay = "";
+
+  for (var i = 0; i < grid.length; i++) {
+    for (var j = 0; j < grid[i].length; j++) {
+      gridDisplay += (grid[i][j] + " ");
+    }
+    drawCoordinates(i);
+    gridDisplay += "\n";
+  }
+
+  console.log(gridDisplay);
+
+}
+
+function drawCoordinates(i) {
+
+  if (i === 2) {
+    gridDisplay += "        N"; //8 espacios
+  }
+  if (i === 3 || i === 5) {
+    gridDisplay += "        |"; //8 espacios
+    if (i === 3) {
+      gridDisplay += "      R: Rover 1";
+    } else {
+      gridDisplay += "      X: Obstacle";
+    }
+  }
+  if (i === 4) {
+    gridDisplay += "     W - - E   T: Rover 2"; //5 espacios
+  }
+  if (i === 6) {
+    gridDisplay += "        S"; //8 espacios
+  }
+
+}
+
+function updateGrid() {
+
+  var posX = myRover.oldPosition.x;
+  var posY = myRover.oldPosition.y;
+
+  grid[posX][posY] = ".";
+  grid[myRover.position[0]][myRover.position[1]] = 'R';
+
+  posX = myRover2.oldPosition.x;
+  posY = myRover2.oldPosition.y;
+
+  grid[posX][posY] = '.';
+  grid[myRover2.position[0]][myRover2.position[1]] = 'T';
+
+  displayGrid();
+
+}
+
+function setupRovers() {
+
+  movementsRover1 = "fffrfflfffbb";
+  movementsRover2 = "ffblfbfflfbf";
+
+  myRover.position = [0, 1];
+  myRover.oldPosition.x = myRover.position[0];
+  myRover.oldPosition.y = myRover.position[1];
+
+  myRover2.position = [1, 2];
+  myRover2.oldPosition.x = myRover2.position[0];
+  myRover2.oldPosition.y = myRover2.position[1];
+
+  console.log("----------Initial Position----------");
+  updateGrid();
+
+}
+
+function goRover() {
+  //PRE:- Same numbers of movement for each Rover.
+
+  setupRovers();
+
+  for (var i = 0; i < movementsRover1.length; i++) {
+
+    doMovement(movementsRover1[i], myRover);
+    doMovement(movementsRover2[i], myRover2);
+    updateGrid();
+
+  }
+}
+
+//goRover();
---
+++ rover.js
@@ -0,0 +1,262 @@
+//    N
+//    |
+// W - - E
+//    |
+//    S
+
+
+function Rover(pos, dir) {
+
+  this.position = pos || [0,0];
+  this.dir = dir || 'N';
+
+}
+
+Rover.prototype.goForward = function() {
+  
+}
+
+Rover.prototype.goBackward = function() {
+  switch (this.direction) {
+    case 'N':
+      this.goSouth();
+      break;
+    case 'E':
+      this.goWest();
+      break;
+    case 'S':
+      this.goNorth();
+      break;
+    case 'W':
+      this.goEst();
+      break;
+  }
+}
+
+Rover.prototype.goForward = function() {
+  switch (this.direction) {
+    case 'N':
+      this.goNorth();
+      break;
+    case 'E':
+      this.goEst();
+      break;
+    case 'S':
+      this.goSouth();
+      break;
+    case 'W':
+      this.goWest();
+      break;
+  }
+}
+
+Rover.prototype.turnLeft = function() {
+  switch (this.direction) {
+    case 'N':
+      this.toWest();
+      break;
+    case 'E':
+      this.toNorth();
+      break;
+    case 'S':
+      this.toEst();
+      break;
+    case 'W':
+      this.toSouth();
+      break;
+  }
+}
+}
+
+Rover.prototype.turnRigth = function() {
+  switch (this.direction) {
+    case 'N':
+      this.toEst();
+      break;
+    case 'E':
+      this.toSouth();
+      break;
+    case 'S':
+      this.toWest();
+      break;
+    case 'W':
+      this.toNorth();
+      break;
+  }
+}
+
+var myRover = {
+  position: [0,0],
+  oldPosition: {},
+  direction: 'N',
+
+  turnRigth: function() {
+
+    this.report = "directioned";
+    switch(this.direction) {
+      case 'N':
+        this.toEst();
+        break;
+      case 'E':
+        this.toSouth();
+        break;
+      case 'S':
+        this.toWest();
+        break;
+      case 'W':
+        this.toNorth();
+        break;
+    }
+  },
+
+  toNorth: function() { this.direction = "N"; },
+
+  toEst: function() { this.direction = "E"; },
+
+  toWest: function() { this.direction = "W"; },
+
+  toSouth: function() { this.direction = "S"; },
+
+  goNorth: function() {
+
+    if (this.canMove("N")) {
+
+      this.saveOldPosition();
+      if (this.isOnTheEdge("N")) {this.position[0] = 9;}
+      else{ this.position[0]--; }
+      this.report = "moved";
+
+    }
+    else {
+
+      this.report = "blocked";
+
+    }
+  },
+
+  saveOldPosition: function() {
+
+    this.oldPosition.x = this.position[0];
+    this.oldPosition.y = this.position[1];
+
+  },
+
+  goEst: function() {
+
+    if (this.canMove("E")) {
+      this.saveOldPosition();
+      if (this.isOnTheEdge("E")) {this.position[1] = 0;}
+      else { this.position[1]++; }
+      this.report = "moved";
+
+    }
+
+    else {
+
+      this.report = "blocked";
+
+    }
+  },
+
+  goSouth: function() {
+
+    if (this.canMove("S")) {
+      this.saveOldPosition();
+      if (this.isOnTheEdge("S")) { this.position[0] = 0; }
+      else{ this.position[0]++; }
+      this.report = "moved";
+
+    }
+
+    else {
+
+      this.report = "blocked";
+
+    }
+
+  },
+
+  goWest: function() {
+
+    if (this.canMove("W")) {
+      this.saveOldPosition();
+      if(this.isOnTheEdge("W")) { this.position[1] = 9;}
+      else {this.position[1]--;}
+      this.report = "moved";
+
+    }
+    else {
+
+      this.report = "blocked";
+
+    }
+  },
+
+  isOnTheEdge: function(toDirection){
+
+    switch(toDirection) {
+      case 'N':
+        if (this.position[0] === 0) {return true; }
+        return false;
+      case 'E':
+        if (this.position[1] === 9) {return true; }
+        return false;
+      case 'S':
+        if (this.position[0] === 9) {return true; }
+        return false;
+      case 'W':
+        if (this.position[1] === 0) {return true; }
+        return false;
+    }
+
+  },
+
+  canMove: function(toDirection) {
+
+    var positionX = this.position[0];
+    var positionY = this.position[1];
+    switch(toDirection) {
+      case 'N':
+        if (this.isOnTheEdge('N')) {positionX = 10;}
+        if (grid[--positionX][positionY] === "X" || this.bumpWithAnotherRover(positionX, positionY)) {return false; }
+        return true;
+      case 'E':
+        if (this.isOnTheEdge('E')) {positionY = -1;}
+        if (grid[positionX][++positionY] === "X" || this.bumpWithAnotherRover(positionX, positionY)) {return false; }
+        return true;
+      case 'S':
+        if (this.isOnTheEdge('S')) {positionX = -1;}
+        if (grid[++positionX][positionY] === "X" || this.bumpWithAnotherRover(positionX, positionY)) {return false; }
+        return true;
+      case 'W':
+        if (this.isOnTheEdge('W')) {positionY = 10;}
+        if (grid[positionX][--positionY] === "X" || this.bumpWithAnotherRover(positionX, positionY)) {return false; }
+        return true;
+    }
+  },
+
+  bumpWithAnotherRover: function(posX, posY) {
+
+    if (posX === myRover2.position[0] && posY === myRover2.position[1]) { return true; }
+    return false;
+
+  },
+
+  showReport: function() {
+
+    switch(this.report) {
+      case "directioned":
+        console.log("New Rover1 Direction to: "+ this.direction + "; at Position: [" + this.position[0] + ", " + this.position[1] + "]");
+        break;
+      case "blocked":
+        console.log("Rover1 Blocked at Position: [" + this.position[0] + ", " + this.position[1] + "]; with Direction: " + this.direction);
+        break;
+      case "moved":
+        console.log("New Rover1 Position: [" + this.position[0] + ", " + this.position[1] + "]");
+        break;
+    }
+
+
+  }
+
+};
